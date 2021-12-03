3e1693dc7c18c07bb6f1a8e511c842a517d18a09
Devedse <davydavidse@gmail.com>
2021-12-03 02:56:53 +0100

    Next problem remains

--- src/DeveRdpConnector/DeveRdpConnector/ViewModels/MainWindowViewModel.cs
+++ src/DeveRdpConnector/DeveRdpConnector/ViewModels/MainWindowViewModel.cs
@@ -8,16 +8,19 @@ namespace DeveRdpConnector.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public IEnumerable<string> EmptyCollection => Enumerable.Repeat<string>("", 1);
+        public string Test => "Hello test";
+
+        public IEnumerable<string> CollectionWithEmptyItem => Enumerable.Repeat("", 1);
 
         public ObservableCollection<ServerInfoGroup> Servers { get; set; } = new ObservableCollection<ServerInfoGroup>();
-        public IEnumerable<string> Environments => EmptyCollection.Concat(Servers.Select(t => t.Environment).Distinct());
-        public IEnumerable<string?> Streams => EmptyCollection.Concat(Servers.Select(t => t.Stream + "").Distinct());
+        public IEnumerable<string> Environments => CollectionWithEmptyItem.Concat(Servers.Select(t => t.Environment).Distinct());
+        public IEnumerable<string?> Streams => CollectionWithEmptyItem.Concat(Servers.Select(t => t.Stream + "").Distinct());
 
         public MainWindowViewModel()
         {
             Servers.Clear();
-            var serverInfos = ServerInfoLoader.ObtainServerInfos(new List<string>() { "ServerNames-1-TLS.txt" });
+            //var serverInfos = ServerInfoLoader.ObtainServerInfos(new List<string>() { "ServerNames-1-TLS.txt" });
+            var serverInfos = ServerInfoLoader.ObtainServerInfos(new List<string>() { @"C:\XGitPrivate\DeveRdpConnector\src\DeveRdpConnector\DeveRdpConnector\ServerNames-1-TLS.txt" });
             foreach (var serverInfo in serverInfos)
             {
                 Servers.Add(serverInfo);
